cb47e7bae9c2d2213396cbf83e42135f106b3756
Pato TB <patriciotubio@gmail.com>
2022-11-23 22:34:09 -0300

    add index.html with forms

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__) # Lo convierte en Flask APP
 
-@app.route("/")
+db = sqlite3.connect("db/paises.sqlite")
+cur = db.cursor()
+
+# https://inloop.github.io/sqlite-viewer/#
+
+@app.route("/", methods=["GET","POST"])
 def index():
-	return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        area_data_form = request.form.get("area_data_form")
+        res = cur.execute("SELECT from ")
+        return render_template("results.html", cur=cur, area_data_form=area_data_form)
